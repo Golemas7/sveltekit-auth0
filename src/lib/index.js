0f2cf67736c411bb408a88f812c2672e2b571b04
Simas <simas.leonavicius@devbridge.com>
2023-01-04 15:28:52 +0200

    Update how items are exported.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,25 @@
 // Reexport your entry components here
+
+import auth, {
+	getUser,
+	getUserValue,
+	getIsAuthenticated,
+	getIsAuthenticatedValue,
+	getPopupOpen,
+	getPopupOpenValue,
+	getError,
+	getErrorValue
+} from './auth0/auth-service';
+
+export {
+	getUser,
+	getUserValue,
+	getIsAuthenticated,
+	getIsAuthenticatedValue,
+	getPopupOpen,
+	getPopupOpenValue,
+	getError,
+	getErrorValue
+};
+
+export default auth;
