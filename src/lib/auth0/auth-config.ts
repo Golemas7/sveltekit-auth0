9ac1353845e1fb533c8f6b9a776f7137f72d4051
Simas <simas.leonavicius@devbridge.com>
2023-01-04 16:29:31 +0200

    Update the naming of env variables for sveltekit to work with them.

--- src/lib/auth0/auth-config.ts
+++ src/lib/auth0/auth-config.ts
@@ -1,9 +1,9 @@
 import type Auth0Config from './models/auth-config.model';
 
 const config: Auth0Config = {
-	domain: import.meta.env['VITE_PUBLIC_AUTH0_DOMAIN'],
-	clientId: import.meta.env['VITE_PUBLIC_AUTH0_CLIENT_ID'],
-	redirectUri: import.meta.env['VITE_PUBLIC_AUTH0_REDIRECT_URI']
+	domain: process.env['PUBLIC_VITE_AUTH0_DOMAIN'] as string,
+	clientId: process.env['PUBLIC_VITE_AUTH0_CLIENT_ID'] as string,
+	redirectUri: process.env['PUBLIC_VITE_AUTH0_REDIRECT_URI'] as string
 };
 
 export default config;
