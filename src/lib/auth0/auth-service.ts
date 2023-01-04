0b6a0fe353642a38c565450fc542fcad245d47d5
Simas <simas.leonavicius@devbridge.com>
2023-01-04 16:10:26 +0200

    Update how one of the types is imported.

--- src/lib/auth0/auth-service.ts
+++ src/lib/auth0/auth-service.ts
@@ -1,9 +1,9 @@
 import config from './auth-config.js';
-import createAuth0Client, { Auth0Client } from '@auth0/auth0-spa-js';
+import createAuth0Client from '@auth0/auth0-spa-js';
 import { get } from 'svelte/store';
 import { popupOpen, user, isAuthenticated, error, client } from '../store.js';
 
-import type { PopupLoginOptions, User } from '@auth0/auth0-spa-js';
+import type { PopupLoginOptions, User, Auth0Client } from '@auth0/auth0-spa-js';
 import type { Readable } from 'svelte/store';
 
 async function createClient() {
