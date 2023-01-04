980994060d1c021f3b50778c7f9b9a039658e93f
Simas <simas.leonavicius@devbridge.com>
2023-01-04 16:34:59 +0200

    Update how env variables are imported.

--- src/lib/auth0/auth-config.ts
+++ src/lib/auth0/auth-config.ts
@@ -1,9 +1,9 @@
 import type Auth0Config from './models/auth-config.model';
 
 const config: Auth0Config = {
-	domain: process.env['PUBLIC_VITE_AUTH0_DOMAIN'] as string,
-	clientId: process.env['PUBLIC_VITE_AUTH0_CLIENT_ID'] as string,
-	redirectUri: process.env['PUBLIC_VITE_AUTH0_REDIRECT_URI'] as string
+	domain: import.meta.env['PUBLIC_VITE_AUTH0_DOMAIN'],
+	clientId: import.meta.env['PUBLIC_VITE_AUTH0_CLIENT_ID'],
+	redirectUri: import.meta.env['PUBLIC_VITE_AUTH0_REDIRECT_URI']
 };
 
 export default config;
