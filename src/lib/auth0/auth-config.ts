daff101555d1ea73d5053ba17129ab2b7936295f
Simas Leonavicius <simas.leonavicius@devbridge.com>
2023-01-07 19:07:41 +0200

    Update how env parameters are used for localhost to work again.

--- src/lib/auth0/auth-config.ts
+++ src/lib/auth0/auth-config.ts
@@ -1,8 +1,8 @@
 import type Auth0Config from './models/auth-config.model';
 
-const auth0Domain = import.meta.env ? import.meta.env['PUBLIC_VITE_AUTH0_DOMAIN'] : '';
-const auth0ClientId = import.meta.env ? import.meta.env['PUBLIC_VITE_AUTH0_CLIENT_ID'] : '';
-const auth0RedirectUri = import.meta.env ? import.meta.env['PUBLIC_VITE_AUTH_REDIRECT_URI'] : '';
+const auth0Domain = import.meta.env ? import.meta.env['VITE_AUTH0_DOMAIN'] : '';
+const auth0ClientId = import.meta.env ? import.meta.env['VITE_AUTH0_CLIENT_ID'] : '';
+const auth0RedirectUri = import.meta.env ? import.meta.env['VITE_AUTH_REDIRECT_URI'] : '';
 
 const config: Auth0Config = {
 	domain: auth0Domain,
